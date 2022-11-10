f2bd8c739e20dd5d51197c73e8744ecd1e9ce842
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2022-11-10 18:04:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Mandandando um oi para o amigo")
+
+function mandaMensagem (){
+    console.log("Tudo bem?")
+    console.log("Vou te mandar uma solicitação")
+    console.log("Solicitação recebida")
+}
+
+mandaMensagem()
+
+console.log("Tchau tchau")
